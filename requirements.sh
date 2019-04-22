42f6a9cc1a8ab64c843411e1aec124e2af0ff35f
Lucas Hillebrandt <hillebrandt.lucas@gmail.com>
2019-04-22 08:39:13 -0700

    Adds apt-get update. adds apt-get dist-upgrade. Minor fixes

--- requirements.sh
+++ requirements.sh
@@ -10,6 +10,8 @@ green='\e[0;32m'
 yellow='\e[0;33m'
 nc='\e[0m'
 
+apt-get update && apt-get dist-upgrade -y
+
 programs=("ssh" "git" "python3")
 
 for item in "${programs[@]}"; do
@@ -36,7 +38,6 @@ for item in "${programs[@]}"; do
     fi
 done
 
-
 bits=$(uname -m)
 
 if [[ "x86_64" != "${bits}" ]]; then
@@ -46,12 +47,16 @@ fi
 
 code () {
     $(sh -c 'echo -e "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' &> /dev/null)
+
     echo -e "Fazendo o Download do ${yellow} Visual Studio Code ${nc}"
-    $(curl -sS  --progress-bar https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &> /dev/null)
-    $(mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &> /dev/null)
-    $(apt-get update &> /dev/null)
+    curl -sS  --progress-bar https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &> /dev/null
+
+    mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &> /dev/null
+
+    apt-get update &> /dev/null
+
     echo -e "Installando o ${yellow} Visual Studio Code ${nc}"
-    $(apt-get install code)
+    apt-get install code
 
     if [[ $? -ne 0 ]]; then
         echo -e "${red}[ERRO]${nc} Nao foi possivel instalar o${yellow} Visual Studio Code${nc}. Por favor, contate Lucas Hillebrandt ${yellow}( lucas@lucashillebrandt.com.br )${nc} para tratar este problema."
